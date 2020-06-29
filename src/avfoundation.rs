86bf40b6626a8cf752d626333f246dd6610e7dd9
Vincent Isambart <vincent.isambart@gmail.com>
2020-06-29 16:56:48 +0900

    Rename core to base as it's too close to "coremedia" or "coretext"

--- src/avfoundation.rs
+++ src/avfoundation.rs
@@ -1,4 +1,4 @@
-use crate::core::*;
+use crate::base::*;
 use crate::foundation::*;
 
 #[link(name = "AVFoundation", kind = "framework")]
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![warn(rust_2018_idioms)]
 
 pub mod avfoundation;
-pub mod core;
+pub mod base;
 pub mod foundation;
