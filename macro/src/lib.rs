b81865a04e14906c75d4f4c2966be47ecb729cda
Vincent Isambart <vincent.isambart@gmail.com>
2020-07-26 14:20:59 +0900

    fourcc macro

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -3,7 +3,7 @@
 // TODO:
 // - field names should not be hardcoded.
 
-use quote::{format_ident, quote};
+use quote::{format_ident, quote, ToTokens};
 use syn::punctuated::Punctuated;
 use syn::{parse_macro_input, DeriveInput};
 
@@ -205,5 +205,54 @@ fn nsobjectprotocol_derive(input: DeriveInput) -> Result<proc_macro2::TokenStrea
                 }
             }
         })
+    }
+}
+
+fn fourcc_impl(str_lit: &syn::LitStr) -> syn::Result<proc_macro2::TokenStream> {
+    let text = str_lit.value();
+    let span = str_lit.span();
+
+    if !text.is_ascii() {
+        return Err(syn::Error::new_spanned(
+            str_lit,
+            "must be all ASCII characters",
+        ));
+    }
+    if text.len() != 4 {
+        return Err(syn::Error::new_spanned(
+            str_lit,
+            "must be exactly 4 characters",
+        ));
+    }
+    let value = text.chars().fold(0, |acc, c| acc << 8 | (c as u32));
+
+    let generated_lit_code = format!("0x{:X}u32", value);
+    let generated_lit = syn::LitInt::new(&generated_lit_code, span);
+    Ok(generated_lit.into_token_stream())
+}
+
+#[proc_macro]
+pub fn fourcc(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let lit = parse_macro_input!(input as syn::LitStr);
+    match fourcc_impl(&lit) {
+        Ok(output) => output,
+        Err(err) => err.to_compile_error(),
+    }
+    .into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn fourcc_generated_literal(text: &str) -> String {
+        let lit_str = syn::LitStr::new(text, proc_macro2::Span::call_site());
+        fourcc_impl(&lit_str).unwrap().to_string()
+    }
+
+    #[test]
+    fn fourcc() {
+        assert_eq!(fourcc_generated_literal("soun"), "0x736F756Eu32");
+        assert_eq!(fourcc_generated_literal("text"), "0x74657874u32");
     }
 }
